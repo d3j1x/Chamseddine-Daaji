ecef4a3a0a5d59aad36a68cdd9f844233a2676a0
d3j1x <d3j1x@yahoo.com>
2025-10-09 20:22:13 +0100

    fix navbar

--- src/lib/components/layout/Mynavbar.svelte
+++ src/lib/components/layout/Mynavbar.svelte
@@ -1,5 +1,7 @@
-<header class="flex justify-between items-center sticky top-0 z-50 shadow-xl px-4 py-2  text-center">
-  <img class="flex-1 w-10 h-10 object-contain mx-auto rounded-b-full" src="/images/chamseddine.png" alt="call">
-  <h1 class="flex-1 font-extrabold md:text-xl">شــمــس الــديــن دعــجــي</h1>
-  <div class="flex-1 "><a href="mailto:d3j1x@yahoo.com" class="font-semibold  border  rounded p-1 hover:bg-gray-300 hover:text-blue-600 hover:animate-pulse">الـبـريـد الإلـكـتـرونـي</a></div>
+<header class="flex justify-evenly items-center sticky top-0 z-50 shadow-xl py-2  text-center">
+  <h1 class="flex font-extrabold text-lg">شــمــس الــديــن دعــجــي</h1>
+  <div class="flex ">
+    <a href="mailto:d3j1x@yahoo.com" 
+    class="font-bold  border  rounded p-1 hover:bg-gray-300 hover:text-blue-600 hover:animate-pulse">الـبـريـد الإلـكـتـرونـي</a>
+  </div>
 </header>
