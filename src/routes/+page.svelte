95b56519efda1f1eac0b414d10f80b814e447fb3
d3j1x <d3j1x@yahoo.com>
2025-10-10 21:26:42 +0100

    add about me component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
+<script>
+	import About from "$lib/components/home/About.svelte";
+
+</script>
 <svelte:head>
 <title>شــمــس الــديــن دعــجــي
 </title>
 <meta name="description" content="رائد أعمال" />
 </svelte:head>
+
+
+<About />
